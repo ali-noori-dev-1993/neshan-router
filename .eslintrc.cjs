0bd3ebfeaf19c75d9ecd3d7a836ea604c7cf5435
Ali Noori <alinoorimlm@yahoo.com>
2024-05-08 21:11:13 +0330

    Initial commit: Setup project and create app from scratch

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    rules: {
+        "@typescript-eslint/no-unnecessary-type-constraint": "off",
+        "no-debugger": "off",
+        "react-hooks/exhaustive-deps": "off",
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "react-refresh/only-export-components": ["warn", {allowConstantExport: true}],
+        "@typescript-eslint/ban-types": [
+            "error",
+            {
+                types: {
+                    "{}": false,
+                },
+                extendDefaults: true,
+            },
+        ],
+    },
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# Interview Task for Neshan
+
+This project was created as an interview task for Neshan. The purpose of this task is to demonstrate my skills. If you have any questions or feedback, please feel free to contact me.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/iroon-ila/neshan.git`
+2. Navigate to the project directory: `cd project-directory`
+3. Install dependencies: `npm install`
+
+## Usage
+
+1. Run the development server: `npm start`
+2. Open your browser and navigate to `http://localhost:3000`
