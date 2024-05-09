5f31901a5bcd364f0739a7358f7bf0f316c411a7
Ali Noori <alinoorimlm@yahoo.com>
2024-05-09 03:50:17 +0330

    feat: Implement Neshan Maps integration in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { MapComponent, MapTypes } from "@neshan-maps-platform/mapbox-gl-react";
+import "@neshan-maps-platform/mapbox-gl-react/dist/style.css";
+
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="w-full h-full">
+      <MapComponent
+        options={{
+          mapKey: "web.fcd433b8ddb9445eaf875e0fdc7f8c71",
+          mapType: MapTypes.neshanRaster,
+          zoom: 11,
+          center: [59.6067, 36.2972],
+        }}
+      />
+    </div>
+  );
 }
